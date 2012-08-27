3baa5e8e154616cec472254a68182fc8869ac4b0
Kevin Moore <kevin@thinkpixellab.com>
2012-08-26 22:33:51 -0400

    Contents of html from commit 0998aee2

--- canvas/sweeper_canvas.dart
+++ canvas/sweeper_canvas.dart
@@ -4,13 +4,22 @@
 #import('../../lib/canvas.dart');
 
 main(){
+  final int w = 16, h = 16, m = 40;
+
   final CanvasElement sweeperTable = query('#sweeperCanvas');
   final Element minesLeftDiv = query('#minesLeft');
   final Element gameStateDiv = query('#gameState');
   final Element clockDiv = query('#clock');
-  final gameRoot = new GameRoot(sweeperTable, minesLeftDiv,
-      gameStateDiv, clockDiv);
+  final gameRoot = new GameRoot(w, h, m,
+      sweeperTable, minesLeftDiv, gameStateDiv, clockDiv);
 
   final ButtonElement newGameButton = query('#newGame');
   newGameButton.on.click.add((args) => gameRoot.newGame());
+
+  // disable touch events
+  window.on.touchMove.add(_onTouchMove);
+}
+
+void _onTouchMove(TouchEvent args) {
+  args.preventDefault();
 }
